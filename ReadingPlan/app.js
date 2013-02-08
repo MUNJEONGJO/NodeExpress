63a7a7f5c8febf983f94906e4d447f78ca80f12c
MUNJEONGJO <dnpsel04@naver.com>
2013-02-08 15:32:56 +0600

    commit!

--- ReadingPlan/app.js
+++ ReadingPlan/app.js
@@ -43,12 +43,27 @@ app.get('/byYear', function (request, response) {
 	});
 });
 
+app.get('/byYear/:category', function (request, response, next) {  
+	response.render('year_list', {
+		title: request.params.category + 'Year page',
+		layout: 'layout_sub.jade'
+	});
+});
+
 app.get('/byField', function (request, response) { 
 	response.render('field', {
 		title: 'Field page',
 		layout: 'layout_sub.jade'
 	});
 });
+
+app.get('/byField/:category', function (request, response, next) { 
+	response.render('field_list', {
+		title: request.params.category + 'Field page',
+		layout: 'layout_sub.jade'
+	});
+});
+
 
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
--- ReadingPlan/public/javascripts/main.js
+++ ReadingPlan/public/javascripts/main.js
@@ -15,4 +15,71 @@ $(function() {
 		$(location).attr('href', url);
 
 	});
+
+	$("#addYear").click(function(){
+
+		var reply = prompt('Hi, there! Please input new year');
+
+		if (reply)
+		{
+			var $something = $('<button/>').attr({class:'btn btn-primary category',id:reply,onclick:'viewYearList(this.id)',draggable:true,ondragstart:'dragStart(event)'});
+
+			$something.html(reply);
+
+			$('#left').append($something);
+		}
+		else
+		{
+			return false;
+		}
+	});
+
+	$("#addField").click(function(){
+
+		var reply = prompt('Hi, there! Please input new field');
+
+		if (reply)
+		{
+			var $something = $('<button/>').attr({class:'btn btn-primary',id:reply,onclick:'viewFieldList(this.id)',draggable:true,ondragstart:'dragStart(event)'});
+
+			$something.html(reply);
+
+			$('#left').append($something);
+		}
+		else
+		{
+			return false;
+		}
+
+	});
+
 });
+
+function dragStart(ev){
+	ev.dataTransfer.setData('text/plain', ev.target.id);
+}
+
+function dragOver(ev){
+	ev.preventDefault();
+}
+
+function dragDrop(ev){
+	var parent = document.getElementById('left');
+	ev.preventDefault();
+	var data = ev.dataTransfer.getData('text/plain');
+	parent.removeChild(document.getElementById(data));
+}
+
+function viewYearList(category){
+	//alert(category);
+	var url = "/byYear/"+category;
+			
+		$(location).attr('href', url);
+}
+
+function viewFieldList(category){
+	//alert(category);
+	var url = "/byField/"+category;
+			
+		$(location).attr('href', url);
+}
